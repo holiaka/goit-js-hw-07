b07751bcd2fff76ec9f53529ba96d753f1b2ebc7
Dmytrii Holiaka <holyaka_d_m@ukr.net>
2022-10-30 01:34:36 +0300

    Prepared first task in HM #7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+//**************************************************** */
+
+// Create gallery of images/photos
+
+const imageBoxRef = document.querySelector(".gallery");
+
+const imageElements = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"/></a></div>`
+);
+
+imageBoxRef.insertAdjacentHTML("beforeend", imageElements.join(""));
+
+// Add event listners and functions
+
+imageBoxRef.addEventListener("click", onImageClick);
+
+function onImageClick(evt) {
+  evt.preventDefault();
+  controlModalImage(evt.target.dataset.source);
+}
+
+function controlModalImage(link) {
+  const statusGalleryImage = basicLightbox.create(`
+    <img src="${link}" width="800" height="600">
+    `);
+  statusGalleryImage.show();
+
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {      
+      return statusGalleryImage.close();
+    }
+  });
+}
