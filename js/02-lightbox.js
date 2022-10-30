42eaa0e29307f93c806400ee17af1d446db4663c
Dmytrii Holiaka <holyaka_d_m@ukr.net>
2022-10-30 12:05:55 +0200

    Final version HM #7 on JS before mentor review

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+//**************************************************** */
+
+// Create gallery of images/photos
+
+const imageBoxRef = document.querySelector(".gallery");
+
+const imageElements = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}"
+    alt="${description}" /></a>`
+);
+
+imageBoxRef.insertAdjacentHTML("beforeend", imageElements.join(""));
+
+// Added the functional from SimpleLightbox's library
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+const bodyRef = document.querySelector("body");
+
+gallery.on("shown.simplelightbox", function () {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+});
+
+gallery.on("changed.simplelightbox", function () {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+});
+
+gallery.on("close.simplelightbox", function () {
+  bodyRef.style.backgroundColor = "#ffffff";
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
